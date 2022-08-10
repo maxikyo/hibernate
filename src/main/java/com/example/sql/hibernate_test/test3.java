4e1fe72955b557c17a9cd35f7db1e3867934c086
maxikyo <holovanovmax@gmail.com>
2022-08-10 21:58:41 +0200

    new t3

---
+++ src/main/java/com/example/sql/hibernate_test/test3.java
@@ -0,0 +1,27 @@
+package com.example.sql.hibernate_test;
+
+
+import com.example.sql.hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class test3 {
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+            session.getTransaction().commit();
+            System.out.println("done!");
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
