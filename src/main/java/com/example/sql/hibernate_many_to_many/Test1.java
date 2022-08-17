baeecbff70efc1537ffffc52b8e3d2727ea8643e
maxikyo <holovanovmax@gmail.com>
2022-08-17 21:27:07 +0200

    add to table

--- src/main/java/com/example/sql/hibernate_many_to_many/Test1.java
+++ src/main/java/com/example/sql/hibernate_many_to_many/Test1.java
@@ -1,5 +1,46 @@
 package com.example.sql.hibernate_many_to_many;
 
 
+import com.example.sql.hibernate_many_to_many.entity.Child;
+import com.example.sql.hibernate_many_to_many.entity.Section;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Child.class)
+                .addAnnotatedClass(Section.class)
+                .buildSessionFactory();
+
+        Session session = null;
+
+        try{
+            session = factory.getCurrentSession();
+
+            Section section1 = new Section("Football");
+            Child child1 = new Child("Max", 7);
+            Child child2 = new Child("Masha", 7);
+            Child child3 = new Child("Vasya", 7);
+            section1.addChildToSection(child1);
+            section1.addChildToSection(child2);
+            section1.addChildToSection(child3);
+
+            session.beginTransaction();
+
+            session.save(section1);
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+
+    }
+        finally {
+            session.close();
+            factory.close();
+        }
+
+    }
 }
