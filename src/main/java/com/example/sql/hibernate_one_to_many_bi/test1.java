eca6b052ea6ddfd3f3bb62c4240544a4879fee8e
maxikyo <holovanovmax@gmail.com>
2022-08-17 19:01:48 +0200

    add new table dep/add new employee

--- src/main/java/com/example/sql/hibernate_one_to_many_bi/test1.java
+++ src/main/java/com/example/sql/hibernate_one_to_many_bi/test1.java
@@ -1,8 +1,9 @@
 package com.example.sql.hibernate_one_to_many_bi;
 
 
-import com.example.sql.hibernate_one_to_one.entity.Detail;
-import com.example.sql.hibernate_one_to_one.entity.Employee;
+
+import com.example.sql.hibernate_one_to_many_bi.Entity.Department;
+import com.example.sql.hibernate_one_to_many_bi.Entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -13,14 +14,24 @@ public class test1 {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
-                .addAnnotatedClass(Detail.class)
+                .addAnnotatedClass(Department.class)
                 .buildSessionFactory();
 
         Session session = null;
         try {
             session = factory.getCurrentSession();
-            session.beginTransaction();
+            Department dep = new Department("IT", 4000,
+                    800);
+            Employee emp1 = new Employee("Maxim", "Holovanov",
+                    800);
+            Employee emp2 = new Employee("Elena", "Smirnova",
+                    1000);
 
+            dep.addEmployeeToDepartment(emp1);
+            dep.addEmployeeToDepartment(emp2);
+
+            session.beginTransaction();
+            session.save(dep);
 
             session.getTransaction().commit();
 
